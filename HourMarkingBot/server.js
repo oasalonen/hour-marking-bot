39361878fafd856accbc0c16ebe4d0a07decff3f
Olli Salonen <olli.salonen@futurice.com>
2017-10-02 08:26:45 +0200

    Local server functionality to get rid of emulator dependency.

--- HourMarkingBot/server.js
+++ HourMarkingBot/server.js
@@ -1,16 +1,25 @@
-var restify = require('restify');
-var Logger = require("bunyan");
 var Bot = require("./bot");
+var nconf = require("nconf");
+var restify = require('restify');
 
-// Setup Restify Server
-//var server = restify.createServer({
-//    log: Logger.createLogger({
-//        name: "bot",
-//        level: "trace"
-//    })
-//});
+nconf.env().argv();
+
+
 var server = restify.createServer();
 server.post('/api/messages', Bot.connector.listen());
-server.listen(process.env.port || 3978, function () {
+server.listen(nconf.get('port') || 3978, function () {
     console.log('%s listening to %s', server.name, server.url);
 });
+
+if (nconf.get("BOT_SERVER_TYPE") === "local") {
+    const directline = require("offline-directline");
+    const express = require("express");
+    const path = require("path");
+
+    const app = express();
+    directline.initializeRoutes(app, "http://localhost:3000", "http://localhost:3978/api/messages");
+
+    app.get("/", (req, res) => {
+        res.sendFile(path.join(__dirname + "/index.html"));
+    });
+}
